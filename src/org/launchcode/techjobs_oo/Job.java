d675edd27ae2f001a9ea7d28fa4cb2faef9910f2
Marlaina <mlwunder@mtu.edu>
2020-09-20 23:19:30 -0500

    Added comments for clarity

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -93,9 +93,17 @@ public class Job {
         this.coreCompetency = coreCompetency;
     }
 
+
+    //couldn't think of a clean way to DRY this up, mostly due to strings
+    // needing the tags at the beginning and Java not communicating variable names.
     public String toString(){
+        // starts the string we'll be returning.
         String toPrint ="\nID: "+ id + "\n";
+
+        //keeps track of how much data ISN'T provided.
         int oops = 0;
+
+        //checks to see if Name is NULL.
         if(name == null || name.equals("")) {
             toPrint += "Name: Data not available\n";
             oops++;
@@ -103,6 +111,8 @@ public class Job {
         else {
             toPrint += "Name: "+ name + "\n";
         }
+
+        //checks to see is employer is NULL.
         if(employer == null || employer.toString().equals("")) {
             toPrint += "Employer: Data not available\n";
             oops++;
@@ -111,6 +121,7 @@ public class Job {
             toPrint += "Employer: "+ employer.toString() + "\n";
         }
 
+        //checks to see if location is NULL.
         if(location == null || location.toString().equals("")) {
             toPrint += "Location: Data not available\n";
             oops++;
@@ -119,6 +130,7 @@ public class Job {
             toPrint += "Location: "+ location.toString() + "\n";
         }
 
+        //checks to see if position is NULL
         if(positionType == null || positionType.toString().equals("")) {
             toPrint += "Position Type: Data not available\n";
             oops++;
@@ -127,6 +139,7 @@ public class Job {
             toPrint += "Position Type: "+ positionType.toString() + "\n";
         }
 
+        //checks to see if coreCompetency is NULL
         if(coreCompetency == null || coreCompetency.toString().equals("")) {
             toPrint += "Core Competency: Data not available\n";
             oops++;
@@ -134,6 +147,8 @@ public class Job {
         else {
             toPrint += "Core Competency: "+ coreCompetency.toString() + "\n";
         }
+
+        //If all fields are NULL, return an error.
         if(oops == 5){
             return "OOPS! This job does not seem to exist.";
         }
